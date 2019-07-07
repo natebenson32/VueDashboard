fb66d2622e032fc164716c7f3bf081a816e98c01
Nate Benson <nathanbenson32@gmail.com>
2019-07-07 02:30:12 -0400

    Added text to the home button that animates away when sidebar collapses

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -2,7 +2,10 @@
   <div class="dashboard">
     <div class="sidebar-container" :class="{ 'sidebar-closed': !sidebarOpen }">
       <div class="home-btn">
-        <div class="home-icon" />
+        <div class="home-container">
+          <div class="home-icon" />
+          <div class="home-text" :class="{ 'home-closed': !sidebarOpen }">HAVEN</div>
+        </div>
       </div>
       <SideNav />
       <!-- <div class="footer"></div> -->
@@ -58,10 +61,28 @@ export default {
   background: rgb(30, 61, 100);
   cursor: pointer;
 }
+.home-container {
+  height: 100%;
+  display: flex;
+  justify-content: center;
+}
 .home-icon {
+  width: 40px;
   height: 100%;
   background: url('../assets/haven-logo.svg') center center no-repeat;
-  background-size: 40px;
+  background-size: 30px;
+}
+.home-text {
+  width: 100px;
+  font-size: 28px;
+  line-height: 60px;
+  color: white;
+  overflow: hidden;
+  transition: width 0.4s;
+}
+.home-closed {
+  width: 0;
+  transition: width 0.4s;
 }
 .footer {
   height: 60px;
